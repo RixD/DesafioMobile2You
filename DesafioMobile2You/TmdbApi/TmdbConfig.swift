6ec3cbf96bd7b8a7aa12cfc3245b4301728888f4
Ricardo Brancaglion <ricardo3845@gmail.com>
2020-08-16 03:39:58 -0300

    Added TmdbConfig static configuration values

---
+++ DesafioMobile2You/TmdbApi/TmdbConfig.swift
@@ -0,0 +1,16 @@
+//
+//  TmdbConfig.swift
+//  DesafioMobile2You
+//
+//  Created by Ricardo H. T. N. Brancaglion on 16/08/20.
+//  Copyright © 2020 Ricardo H. T. N. Brancaglion. All rights reserved.
+//
+
+import Foundation
+
+struct TmdbConfig {
+    static let tmdbBaseUrl = "https://api.themoviedb.org"
+    static let tmdbBaseImageUrl = "https://image.tmdb.org"
+    static let tmdbApiKey = "ee61a4deb5f965977f1f534b95b53590"
+    static let version: Int = 3
+}
