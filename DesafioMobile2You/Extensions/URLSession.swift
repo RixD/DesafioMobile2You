ce2493649a227fe83f508806883eea5309aaa6c7
Ricardo Brancaglion <ricardo3845@gmail.com>
2020-08-16 04:29:35 -0300

    Added an Extension to be able to perform DataTask using the TmdbApi

---
+++ DesafioMobile2You/Extensions/URLSession.swift
@@ -0,0 +1,41 @@
+//
+//  URLSession.swift
+//  DesafioMobile2You
+//
+//  Created by Ricardo H. T. N. Brancaglion on 16/08/20.
+//  Copyright © 2020 Ricardo H. T. N. Brancaglion. All rights reserved.
+//
+
+import Foundation
+
+extension URLSession {
+    func dataTask(with url: URL, result: @escaping (Swift.Result<(URLResponse, Data), Error>) -> Void) -> URLSessionDataTask {
+        return dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                result(.failure(error))
+                return
+            }
+            guard let response = response, let data = data else {
+                let error = NSError(domain: "error", code: 0, userInfo: nil)
+                result(.failure(error))
+                return
+            }
+            result(.success((response, data)))
+        }
+    }
+    
+    func dataTask(with urlRequest: URLRequest, result: @escaping (Swift.Result<(URLResponse, Data), Error>) -> Void) -> URLSessionDataTask {
+        return dataTask(with: urlRequest) { (data, response, error) in
+            if let error = error {
+                result(.failure(error))
+                return
+            }
+            guard let response = response, let data = data else {
+                let error = NSError(domain: "error", code: 0, userInfo: nil)
+                result(.failure(error))
+                return
+            }
+            result(.success((response, data)))
+        }
+    }
+}
