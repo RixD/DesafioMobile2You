f1e95bcb2b717f5e0d8cd3c23d3c945e5b6e81fd
Ricardo Brancaglion <ricardo3845@gmail.com>
2020-08-16 23:48:10 -0300

    Added UITableView util extensions

---
+++ DesafioMobile2You/Extensions/UITableView-Util.swift
@@ -0,0 +1,23 @@
+//
+//  UITableView-Util.swift
+//  DesafioMobile2You
+//
+//  Created by Ricardo H. T. N. Brancaglion on 16/08/20.
+//  Copyright © 2020 Ricardo H. T. N. Brancaglion. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async { self.reloadData() }
+    }
+    
+
+    func removeExcessCells() {
+        tableFooterView = UIView(frame: .zero)
+    }
+}
+
+
